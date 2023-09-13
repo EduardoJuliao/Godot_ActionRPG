a3db2576dbc3a60e00a67314ad658cac24313910
Eduardo Julião <eduardo.juliaojr@outlook.com>
2023-09-13 21:22:58 +0100

    PLayer attack

--- Player/Player.gd
+++ Player/Player.gd
@@ -4,11 +4,36 @@ const ACCELERATION = 500;
 const MAX_SPEED = 80;
 const FRICTION = 500;
 
+enum {
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
+var state = MOVE;
+
+
 @onready var animationPlayer = $AnimationPlayer;
 @onready var animationTree = $AnimationTree;
 @onready var animationState = animationTree.get("parameters/playback");
 
+func _ready():
+	animationTree.active = true;
+
 func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state(delta);
+		ATTACK:
+			attack_State(delta);
+		ROLL:
+			pass;
+			
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK;
+	
+	
+func move_state(delta):
 	var input_vector = Vector2.ZERO;
 	
 	input_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left");
@@ -18,6 +43,7 @@ func _physics_process(delta):
 	if input_vector != Vector2.ZERO:
 		animationTree.set("parameters/Idle/blend_position", input_vector);
 		animationTree.set("parameters/Run/blend_position", input_vector);
+		animationTree.set("parameters/Attack/blend_position", input_vector);
 		animationState.travel("Run");
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta);
 	else:
@@ -25,3 +51,10 @@ func _physics_process(delta):
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta);
 		
 	move_and_slide();
+
+func attack_State(delta):
+	velocity = Vector2.ZERO;
+	animationState.travel("Attack");
+
+func attack_animation_finished():
+	state = MOVE;
